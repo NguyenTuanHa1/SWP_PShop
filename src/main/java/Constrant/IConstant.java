430a24266c1b345dca513ed0fe27257f21e9c8eb
Tuan Ha <169113351+NguyenTuanHa1@users.noreply.github.com>
2024-07-21 10:37:24 +0700

    Create IConstant.java

---
+++ src/main/java/Constrant/IConstant.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Constrant;
+
+
+
+/**
+ *
+ * @author A
+ */
+public class IConstant {
+   
+    public static final String FACEBOOK_CLIENT_ID = "";
+    public static final String FACEBOOK_CLIENT_SECRET = "";
+    public static final String FACEBOOK_REDIRECT_URI = "http://localhost:8080/login";
+    public static final String FACEBOOK_LINK_GET_TOKEN="https://www.facebook.com/v20.0/dialog/oauth/access_token";
+    public static final String FACEBOOK_LINK_GET_USER_INFO = "https://graph.facebook.com/me?fields=id,name,email,picture&access_token=";
+}
+
+
